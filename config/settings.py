56514a48d085e0ce7113642574aa7a3ec05bd838
Alexandr Arciuk <alexandr.arciuk@ebs-integrator.com>
2025-08-21 15:43:59 +0300

    milestone4 init

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,7 +66,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "django_db",
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": "5432",
     },
 }
